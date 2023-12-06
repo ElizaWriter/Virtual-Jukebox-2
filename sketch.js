cb975af38e6934cb5c01f83d151959159e60abad
ElizaWriter <144184590+ElizaWriter@users.noreply.github.com>
2023-12-05 16:44:25 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -102,10 +102,10 @@ function mouseClicked () {
   music5.stop();
   music6.stop();
   music1.loop();
-  //music1.play();
-  if (music1.isPlaying()){
-    music1.play();
-  }
+  music1.play();
+  // if (music1.isPlaying()){
+  //   music1.play();
+  // }
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
